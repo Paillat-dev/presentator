7fd2f416dd2ea4a1377f8f2a8447fc0b54677481
Paillat <git@electronique.cc>
2023-02-06 09:11:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -109,13 +109,13 @@ class:
                     handler.write(img_data)
     with open(f"./data/{uid}/{b64}{datenow}/{subject}.md", "w", encoding="utf8") as f: f.write(present)
     #we execute the command to convert the markdown file to a pdf and html file and also generate the first slide image
-    cmd = f"./marp --pdf --allow-local-files ./data/{uid}/{b64}{datenow}/{subject}.md"
+    cmd = f"./marp --pdf --allow-local-files data/{uid}/{b64}{datenow}/{subject}.md"
     os.system(cmd)
     print(cmd)
-    cmd = f"./marp --image png -o ./data/{uid}/{b64}{datenow}/{subject}.png --allow-local-files ./data/{uid}/{b64}{datenow}/{subject}.md"
+    cmd = f"./marp --image png -o ./data/{uid}/{b64}{datenow}/{subject}.png --allow-local-files data/{uid}/{b64}{datenow}/{subject}.md"
     os.system(cmd)
     print(cmd)
-    cmd = f"./marp --html --allow-local-files ./data/{uid}/{b64}{datenow}/{subject}.md"
+    cmd = f"./marp --html --allow-local-files data/{uid}/{b64}{datenow}/{subject}.md"
     os.system(cmd)
     print(cmd)
     #we create an embed with the first slide imageand send it with the pdf file and the markdown file
